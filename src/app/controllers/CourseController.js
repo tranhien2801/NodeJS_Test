0449d0b976a6af588e8eedf70bca2f7d0086d375
admin@XZ <tranhien28012001@gmail.com>
2021-12-01 00:49:19 +0700

    checkbox all

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -20,13 +20,12 @@ class CourseController {
 
     // [POST] /course/store
     store(req, res, next) {
-        const formData = req.body;
-        formData.image =
+        req.body.image =
             'https://img.youtube.com/vi/' + req.body.videoId + '/sddefault.jpg';
-        const course = new Course(formData);
+        const course = new Course(req.body);
         course
             .save()
-            .then(() => res.redirect('/'))
+            .then(() => res.redirect('/me/stored/courses'))
             .catch(next);
     }
 
@@ -49,6 +48,49 @@ class CourseController {
         Course.updateOne({ _id: req.params.id }, formData)
             .then(() => res.redirect('/me/stored/courses'))
             .catch(next);
+    }
+
+    // [DELETE] /course/:id
+    destroy(req, res, next) {
+        Course.delete({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+
+    // [DELETE] /course/:id/force
+    forceDestroy(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+
+    // [PATCH] /course/:id/restore
+    restore(req, res, next) {
+        Course.restore({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+
+    // [POST] /course/handle-form-action
+    handleFromActions(req, res, next) {
+        switch (req.body.action) {
+            // case 'delete':
+            //    // res.json(req.body);
+            //     Course.delete({ _id: { $in: req.body.courseIds  }})
+            //         .then(() => res.redirect('back'))
+            //         .catch(next);
+            //     break;
+            case 'delete':
+                Course.delete({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                break;
+            default:
+                res.json({ message: 'Action is invalid!' });
+        }
+
+        //    res.json(req.body);
+        //console.log(req.body);
     }
 }
 
