b11b87f14b94d1dd458e0d0f811d4eb3bc342d52
admin@XZ <tranhien28012001@gmail.com>
2021-11-30 09:40:09 +0700

    update db

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,18 @@
+const { multipleMongooseToObject } = require('../../util/mongoose');
+const Course = require('../models/Course');
+
 class SiteController {
     // [GET] /
-    home(req, res) {
-        res.render('home');
+    home(req, res, next) {
+        Course.find({})
+            .then((courses) => {
+                res.render('home', {
+                    courses: multipleMongooseToObject(courses),
+                });
+            })
+            .catch(next);
+
+        // res.render('home');
     }
 
     // [GET] /search
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const handlebars = require('express-handlebars');
-var hbs = handlebars.create({ extname: 'hbs' });
-const app = express();
-const port = 3000;
+const hbs = handlebars.create({
+    extname: 'hbs',
+    helpers: {
+        sum: (a, b) => a + b,
+    },
+});
 
 const route = require('./routes');
+const db = require('./config/db');
+
+//Connect to DB
+db.connect();
+
+const app = express();
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,17 +28,19 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride('_method'));
+
 // Http logger
 // app.use(morgan('combined'));
 
 // TEMPLATE ENGINE
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
